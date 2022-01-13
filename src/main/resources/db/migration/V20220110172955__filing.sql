e012753bce2526e667d8aa95ca94297c87e8bba2
yavlash-liza <yavlashliza@gmail.com>
2022-01-13 20:30:15 +0300

    creating migrations to database

---
+++ src/main/resources/db/migration/V20220110172955__filing.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (email, password, first_name, user_role)
+VALUES ('vlad@mail.com', '$2a$12$12aKEfWwKK.EPVyIL3VO2evqHn97GZIXzR3xB02E.o1.GT5shH2Mi', 'vlad', 'user'),
+       ('igor@mail.com', '$2a$12$zD5LfAPS.BuaYx9RudlVPuauPmr7vQLAjMd7RRjAFlAf9fdJpENNm', 'igor', 'admin'),
+       ('liza@mail.com', '$2a$12$CWLXi7KDDqQdhpJcgQ6MjOjKu3imu8WEJ2d7RzWBpJS7RbOq3FG96', 'liza', 'admin');
