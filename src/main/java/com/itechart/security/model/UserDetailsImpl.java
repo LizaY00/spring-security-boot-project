a4bfe865797f187bd14aa86627d72fbfbcccfc4b
yavlash-liza <yavlashliza@gmail.com>
2022-01-13 20:31:20 +0300

    creating secured user details

---
+++ src/main/java/com/itechart/security/model/UserDetailsImpl.java
@@ -0,0 +1,60 @@
+package com.itechart.security.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+
+@Builder
+@Data
+@ToString
+public class UserDetailsImpl implements UserDetails, Serializable {
+    private String username;
+    private String password;
+    private String role;
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority =
+                role != null && role.equals("admin")
+                        ? new SimpleGrantedAuthority("ROLE_ADMIN")
+                        : new SimpleGrantedAuthority("ROLE_USER");
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
