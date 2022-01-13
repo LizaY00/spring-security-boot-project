0df738e3e610635ccb9e6b957f0060085f9c2596
yavlash-liza <yavlashliza@gmail.com>
2022-01-13 20:29:48 +0300

    creating spring boot runner

---
+++ src/main/java/com/itechart/security/ApplicationRunner.java
@@ -0,0 +1,11 @@
+package com.itechart.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
+}
