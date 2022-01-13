410dae34ba01605b6d2ac8ebf65c94d78fdaa563
yavlash-liza <yavlashliza@gmail.com>
2022-01-13 20:30:59 +0300

    creating persist entity

---
+++ src/main/java/com/itechart/security/model/User.java
@@ -0,0 +1,36 @@
+package com.itechart.security.model;
+
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstname;
+
+    @Column(name = "user_role")
+    private String role;
+}
