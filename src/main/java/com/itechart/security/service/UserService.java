edf6f85699bb0bd3dea8d65a601801f06377713a
yavlash-liza <yavlashliza@gmail.com>
2022-01-13 20:32:27 +0300

    creating service

---
+++ src/main/java/com/itechart/security/service/UserService.java
@@ -0,0 +1,31 @@
+package com.itechart.security.service;
+
+import com.itechart.security.converter.UserConverter;
+import com.itechart.security.model.User;
+import com.itechart.security.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(String.format("User with email %s not found", email))
+                );
+        return UserConverter.toUserDetails(user);
+    }
+}
